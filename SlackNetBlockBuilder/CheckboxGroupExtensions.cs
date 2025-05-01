468ecde6ddf234c95bee1db52f15862372e9304f
Darin Carrigg <darin@sidework.co>
2025-05-01 00:34:35 -0500

    Xml Docs

--- SlackNetBlockBuilder/CheckboxGroupExtensions.cs
+++ SlackNetBlockBuilder/CheckboxGroupExtensions.cs
@@ -2,14 +2,32 @@
 
 namespace SlackNet.Blocks;
 
+/// <summary>
+/// Provides a fluent interface for building <see cref="CheckboxGroup"/>. This class is intended to be used as a part
+/// of a <see cref="BlockBuilder"/>.
+/// </summary>
 [PublicAPI]
 public static class CheckboxGroupExtensions
 {
+    /// <summary>
+    /// Add an option to a checkbox group. 
+    /// </summary>
+    /// <param name="builder">The builder instance</param>
+    /// <param name="value">A text object that defines the text shown in the option on the menu. Overflow, select, and multi-select menus can only use plain_text objects, while radio buttons and checkboxes can use mrkdwn text objects. Maximum length for the text in this field is 75 characters.</param>
+    /// <param name="text">A unique string value that will be passed to your app when this option is chosen. Maximum length for this field is 150 characters.</param>
+    /// <param name="description">A plain_text text object that defines a line of descriptive text shown below the text field beside a single selectable item in a select menu, multi-select menu, checkbox group, radio button group, or overflow menu. Checkbox group and radio button group items can also use mrkdwn formatting. Maximum length for the text within this field is 75 characters.</param>
+    /// <returns>The same instance so calls can be chained</returns>
     public static InputElementBuilder<CheckboxGroup> AddOption(this InputElementBuilder<CheckboxGroup> builder,
         string value,
-        string text, PlainText description = null) => builder.Set(x =>
+        string text, PlainText? description = null) => builder.Set(x =>
         x.Options.Add(new Option { Text = text, Value = value, Description = description }));
 
+    /// <summary>
+    /// Indicates whether the element will be set to auto focus within the view object. Only one element can be set to true. Defaults to false.
+    /// </summary>
+    /// <param name="builder">The builder instance</param>
+    /// <param name="focus">If true, the element will be focused when the view is opened. Defaults to false.</param>
+    /// <returns>The same instance so calls can be chained</returns>
     public static InputElementBuilder<CheckboxGroup> FocusOnLoad(this InputElementBuilder<CheckboxGroup> builder,
         bool focus = true)
         => builder.Set(x => x.FocusOnLoad = focus);
