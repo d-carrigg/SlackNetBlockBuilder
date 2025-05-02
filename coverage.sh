a866a836f18fd43439fcfba86ee1fbb71e79f869
Darin Carrigg <d-carrigg@users.noreply.github.com>
2025-05-02 00:58:05 -0500

    Refactor test directory structure and update solution file paths (#3)

--- coverage.sh
+++ coverage.sh
@@ -2,5 +2,5 @@
 set -e
 
 dotnet test --configuration Release --verbosity normal --logger trx --collect:"XPlat Code Coverage" 
-dotnet coverage merge -o Coverage/merged.cobertura.xml -f cobertura UnitTests/**/coverage.cobertura.xml
+dotnet coverage merge -o Coverage/merged.cobertura.xml -f cobertura tests/**/coverage.cobertura.xml
 reportgenerator -reports:"Coverage/merged.cobertura.xml" -targetdir:"Coverage" -reporttypes:Html
