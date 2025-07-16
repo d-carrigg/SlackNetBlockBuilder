e3b479242e8824a7fd69d587134276aee87ab46a
Darin Carrigg <d-carrigg@users.noreply.github.com>
2025-07-16 16:16:05 -0500

    Add multiline, minLength, and maxLength methods to InputElementBuilder

--- src/InputBuilderExtensions.cs
+++ src/InputBuilderExtensions.cs
@@ -22,4 +22,47 @@ public static class InputBuilderExtensions
             builder is null ? throw new ArgumentNullException(nameof(builder)) :
                 builder.Set(x => x.Placeholder = placeholder);
     
+    
+    /// <summary>
+    /// Sets whether the input element should be a multiline text area.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the select menu element.</typeparam>
+    /// <param name="builder">The builder instance.</param>
+    /// <param name="multiline">If true, the input element will be a multiline text area; otherwise, it will be a single-line input.</param>
+    /// <returns>The same builder instance so calls can be chained.</returns>
+    public static InputElementBuilder<TElement> Multiline<TElement>(
+        this InputElementBuilder<TElement> builder,
+        bool multiline = true) where TElement : PlainTextInput
+        => 
+            builder is null ? throw new ArgumentNullException(nameof(builder)) :
+                builder.Set(x => x.Multiline = multiline);
+    
+ 
+    /// <summary>
+    ///  Sets the minimum length of input that the user must provide.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the select menu element.</typeparam>
+    /// <param name="builder">The builder instance.</param>
+    /// <param name="minLength">The minimum length of input required.</param>
+    /// <returns>The same builder instance so calls can be chained.</returns>
+    public static InputElementBuilder<TElement> MinLength<TElement>(
+        this InputElementBuilder<TElement> builder,
+        int minLength) where TElement : PlainTextInput
+        => 
+            builder is null ? throw new ArgumentNullException(nameof(builder)) :
+                builder.Set(x => x.MinLength = minLength);
+    /// <summary>
+    ///  Sets the maximum length of input that the user must provide.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the select menu element.</typeparam>
+    /// <param name="builder">The builder instance.</param>
+    /// <param name="maxLength">The maximum length of input required.</param>
+    /// <returns>The same builder instance so calls can be chained.</returns>
+    public static InputElementBuilder<TElement> MaxLength<TElement>(
+        this InputElementBuilder<TElement> builder,
+        int maxLength) where TElement : PlainTextInput
+        => 
+            builder is null ? throw new ArgumentNullException(nameof(builder)) :
+                builder.Set(x => x.MaxLength = maxLength);
+    
 }
